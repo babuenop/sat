39701b7d6c23ae5ec6ba97560b17d655ebfc7cdc
Alberto <abettetto@gmail.com>
2020-09-16 19:06:12 -0500

    Modified views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,6 +28,9 @@
                 padding: 0 0 1rem 0;
                 border-bottom: 2pt solid #1ab394;
             }
+            img {
+                border: 0
+            }
 
             h1 {
                 margin: 0;
@@ -35,13 +38,64 @@
                 font-size: 2rem;
                 color: #1ab394;
                 display: block;
-                font-size: 2em;
+                font-size: 18px;
                 margin-block-start: 0.67em;
                 margin-block-end: 0.67em;
                 margin-inline-start: 0px;
                 margin-inline-end: 0px;
                 font-weight: bold;
             }
+
+            table.blueTable {
+                
+                width: 100%;
+                text-align: left;
+                border-collapse: collapse;
+            }
+            table.blueTable td, table.blueTable th {
+                border: 1px solid #AAAAAA;
+                padding: 3px 2px;
+            }
+            table.blueTable tbody td {
+                font-size: 12px;
+            }
+            
+            table.blueTable thead {
+                background: #444444;
+                border-bottom: 2px solid #444444;
+            }
+            table.blueTable thead th {
+                font-size: 12px;
+                color: #FFFFFF;
+                
+            }
+            table.blueTable thead th:first-child {
+                border-left: none;
+            }
+
+            table.blueTable tfoot {
+                font-size: 12px;           
+                color: #FFFFFF;
+                border-top: 2px solid #444444;
+            }
+            table.blueTable tfoot td {
+                font-size: 10px;
+            }
+            table.blueTable tfoot .links {
+                text-align: right;
+            }
+            table.blueTable tfoot .links a{
+                display: inline-block;
+                background: #1C6EA4;
+                color: #FFFFFF;
+                padding: 2px 8px;
+                border-radius: 5px;
+            }
+            .client {
+                font-size: 12px;
+                padding: 15px
+            }
+            
 
             .full-height {
                 height: 100vh;
@@ -90,6 +144,8 @@
         @include('layouts.navbars.navbar')    
 
 <main role="main">
+
+
     <div class="container">
         <form>
         <div class="container">
@@ -97,23 +153,7 @@
             <div class="logoholder text-center" background="dark">
             <img src="img/logo.jpg" width="140" height="40">
             </div>
-            
-            <div class="col-4">
-                <p contenteditable="">
-                
-                <strong>Sat Panama</strong><br>
-                Bodega Cirsa<br>
-                Complejo Industrual Parque Sur.<br>
-                </p>
-            </div>
-            
-            <div class="col-4">
-                <p contenteditable="">
-                Fecha: <br>
-                Acta: <br>
-                </p>
-            </div>
-            </div>
+
         </header>
 
     <div class="row section">
@@ -123,33 +163,56 @@
             </div>
         </div>
     </div>
-    <table class="table" table-bordered="">
-				<thead>
-					<tr>
-					<th>Codigo</th>
-					<th>Descripcion</th>
-					<th>Proveedor</th>
-					<th>Estado</th>
-					<th>Cantidad</th>
-					<th>Revisado Por</th>
-					<th>Editar</th>
-					<th>Borrar</th>
-					</tr>
-				</thead>  
+    
+<div>
+<div class="row">
+    <div class="col-6">
+        <p contenteditable="" class="client">
+            <strong>Origen</strong><br>
+            Nombre cliente<br>
+            Nombre emmpresa<br>
+            Dirección empresa<br>
+            Tel empresa
+        </p>
+    </div>
+
+    <div class="col-6">
+        <p contenteditable="" class="client">
+            <strong>Destino</strong><br>
+            [Nombre cliente]<br>
+            [Nombre emmpresa]<br>
+            [Dirección empresa]<br>
+            [Tel empresa]
+        </p>
+    </div>
+    </div>
+</div>
+<br>
+
+    <table class="blueTable" table-bordered="">
+		<thead>
+			<tr>
+				<th>Codigo</th>
+				<th>Descripcion</th>
+				<th>Proveedor</th>
+				<th>Estado</th>
+				<th>Cantidad</th>
+				<th>Revisado Por</th>
+			</tr>
+		</thead>  
 			<tbody>
 				<tr>
-				<td>47792</td>
-				<td>MON-LCD. TL ,NON-LIT 19 INCH DT-TS TOVIS</td>
-				<td>IGT</td>
-				<td>REPARADO</td>
-				<td>10</td>
-				<td>FERNANDEZ ARQUIÑEZ ADALBERTO </td>
-				<td><a href="editar.php?IdActa=678&amp;Codigo=47792"><img src="../img/edit.png" width="25" height="25"></a></td>
-				<td><a href="php/BorrarMaterial.php?IdActa=678&amp;Codigo=47792"><img src="../img/del.png" width="25" height="25"></a></td>
-				
-			</tr></tbody>  
+		    		<td>47792</td>
+	    			<td>MON-LCD. TL ,NON-LIT 19 INCH DT-TS TOVIS</td>
+    				<td>IGT</td>
+				    <td>REPARADO</td>
+				    <td>10</td>
+				    <td>FERNANDEZ ARQUIÑEZ ADALBERTO </td>
+			    </tr>
+            </tbody>  
 			
-			</tr></tbody></table>
+		</tr>
+    </table>
         </form>
     </div>
 
